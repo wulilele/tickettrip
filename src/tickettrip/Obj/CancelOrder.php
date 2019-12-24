71a2e36e456913517b477ec32aaa3f5d969e9563
yuetao <1181200882@qq.com>
2019-12-24 17:09:46 +0800

    修改加签

--- src/tickettrip/Obj/CancelOrder.php
+++ src/tickettrip/Obj/CancelOrder.php
@@ -15,6 +15,6 @@ class CancelOrder extends TicketBase
      * @param $third_tid string 票在旅途云平台订单号
      */
     public function setOrderNo($third_tid){
-        $this->values['paymentTypeId'] = $third_tid;
+        $this->values['orderNo'] = $third_tid;
     }
 }
